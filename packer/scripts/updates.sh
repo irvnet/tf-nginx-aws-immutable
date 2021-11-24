aa0a67e7cf071a1165090e4c830bff97dc110b44
Richard Irving <irvnet@gmail.com>
2021-11-24 09:30:59 -0500

    update html page with ip address

--- packer/scripts/updates.sh
+++ packer/scripts/updates.sh
@@ -6,7 +6,7 @@ sudo apt-get --yes upgrade
 sudo apt-get --yes autoremove
 sudo apt-get --yes install unzip
 
-## choose a random color for the 
+## choose a random color for the page background
 array[0]="Tomato"
 array[1]="Orange"
 array[2]="DodgerBlue"
@@ -15,20 +15,25 @@ array[4]="Gray"
 array[5]="SlateBlue"
 array[6]="Violet"
 array[7]="LightGray"
-
 size=${#array[@]}
 index=$(($RANDOM % $size))
 HEADER_COLOR=${array[$index]}
+echo ">>> RANDOM COLOR IS: ${HEADER_COLOR} <<<"
 
 ## install nginx
 sudo apt-get install nginx -y
 
-## backup default page
+## update index page with a random color
+sed -i "s/@COLOR@/${HEADER_COLOR}/" /tmp/index.html
+
+## update index page with server public ip
+IPADDR=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+sed -i "s/@IPADDR@/${IPADDR}/" /tmp/index.html
+
+## backup/update index page 
+echo ">>> UPDATE INDEX PAGE <<<"
 sudo mv /var/www/html/index.nginx-debian.html /var/www/html/index.nginx-debian.bkp
 sudo mv /tmp/index.html /var/www/html/index.html
-
-## update the index page with a random color
-sed -i "s/@COLOR@/${HEADER_COLOR}/" /tmp/index.html
 
 ## enable/start nginx
 sudo systemctl start nginx
